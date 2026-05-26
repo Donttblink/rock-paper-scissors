8c743ebc41a9ba4d1d7805f39f63c4c1962251ae
Donttblink <jonathon.lerose@gmail.com>
2026-05-26 14:42:07 -0400

    updated randomnumber to be located inside getcomputerchoice function

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;``
-
 function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
   if (randomNumber <= 33) {
     return "Rock";
   } else if (randomNumber >= 33 && randomNumber<= 64) {
