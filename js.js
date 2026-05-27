e2e6251bb489acee6c8c7bccd217d24f545ee4d6
Donttblink <jonathon.lerose@gmail.com>
2026-05-27 08:07:40 -0400

    getHumanChoice is a prompt in the function pulled by console.log

--- js.js
+++ js.js
@@ -2,14 +2,25 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 100) + 1;
   if (randomNumber <= 33) {
     return "Rock";
-  } else if (randomNumber >= 33 && randomNumber<= 66) {
+  } else if (randomNumber >= 33 && randomNumber <= 66) {
     return "Paper";
   } else {
     return "Scissors";
   }
 }
 
-function getHumanChoice("Rock", "Paper", "Scissors") {
-  
-
+function getHumanChoice() {
+  let choice = prompt("Choose Your Character;Rock, Paper, Scissors");
+  return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
