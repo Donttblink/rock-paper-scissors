14bd4fafc36f1e64411c4ef40d36e6d0c83e624e
Donttblink <jonathon.lerose@gmail.com>
2026-06-16 10:17:52 -0400

    removed play game function and made getComputerChoice buckets equal

--- js.js
+++ js.js
@@ -1,72 +1,30 @@
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
-  if (randomNumber <= 33) {
-    return "rock";
-  } else if (randomNumber >= 34 && randomNumber <= 66) {
-    return "paper";
+  const n = Math.floor(Math.random() * 3);
+  if (n === 0) return "rock";
+  if (n === 1) return "paper";
+  return "scissors";
+}
+
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    alert("You Win! Nice!!");
+    humanScore++;
+    return "win";
+  } else if (humanChoice === computerChoice) {
+    alert("Crazyyy, you tied?!");
+    return "tie";
   } else {
-    return "scissors";
+    alert("You Lost Bucko!");
+    computerScore++;
+    return "lose";
   }
 }
 
-function getHumanChoice() {
-  let choice = prompt("Choose Your Character; Rock, Paper, Scissors");
-  return choice.toLowerCase();
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  function playRound(humanChoice, computerChoice) {
-    if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      alert("You Win! Nice!!");
-      humanScore++;
-      return "win";
-    } else if (humanChoice === computerChoice) {
-      alert("Crazyyy, you tied?!");
-      return "tie";
-    } else {
-      alert("You Lost Bucko!");
-      computerScore++;
-      return "lose";
-    }
-  }
-
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-  console.log("Human:", humanSelection, "Computer:", computerSelection);
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log("Human:", humanSelection, "Computer:", computerSelection);
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log("Human:", humanSelection, "Computer:", computerSelection);
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log("Human:", humanSelection, "Computer:", computerSelection);
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log("Human:", humanSelection, "Computer:", computerSelection);
-  playRound(humanSelection, computerSelection);
-  if (humanScore > computerScore) {
-    alert(`You Rule! Score:${humanScore} AI Drools! Score:${computerScore}`);
-  } else if (computerScore > humanScore) {
-    alert(`You're No John Connor! Score:${humanScore} Loser! Score:${computerScore}`);
-  } else {
-    alert(`A Tie Really? Score:${humanScore} Booooo! Score:${computerScore}`);
-  }
-}
-
-playGame();
+const rock = document.querySelector("#rock");
+ btn.addEventListener("click", playRound);
